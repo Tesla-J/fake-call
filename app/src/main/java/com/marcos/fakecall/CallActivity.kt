1f57a689577fdb1c4f5cdffd0899a525a4910d51
Rafael Marcos <rafix995@gmail.com>
2022-01-25 21:41:16 +0100

    CallActivity now plays the default ringtone. It's layout was modified to use the actual method to put it on fullscreen.

--- app/src/main/java/com/marcos/fakecall/CallActivity.kt
+++ app/src/main/java/com/marcos/fakecall/CallActivity.kt
@@ -9,12 +9,15 @@ import android.view.WindowManager
 
 import android.widget.ImageButton
 
-import android.graphics.Color
+import android.media.MediaPlayer
+
+import android.provider.Settings.System
 
 class CallActivity : AppCompatActivity() {
 
     var mAcceptCall:ImageButton? = null
     var mDenyCall:ImageButton? = null
+    var ringtonePlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +26,23 @@ class CallActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_call)
 
-        mAcceptCall = findViewById(R.id.accept_call) as ImageButton
-        mDenyCall = findViewById(R.id.deny_call) as ImageButton
+        //mAcceptCall = findViewById(R.id.accept_call) as ImageButton
+        //mDenyCall = findViewById(R.id.deny_call) as ImageButton
 
         //mAcceptCall!!.setBackgroundColor(Color.GREEN)
         //mDenyCall!!.setBackgroundColor(Color.RED)
+
+        //play the default ringtone
+        ringtonePlayer = MediaPlayer.create(this, System.DEFAULT_RINGTONE_URI)
+        ringtonePlayer?.start()
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        if (ringtonePlayer?.isPlaying() == true){
+            ringtonePlayer?.stop()
+            ringtonePlayer?.release()
+        }
     }
 
     fun onClick(view:View) = this.finish()
